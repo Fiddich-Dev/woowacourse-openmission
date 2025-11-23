52b5b34399349898f659eaa93d4b6324497f673b
Fiddich <hiws99@naver.com>
2025-11-23 11:50:21 +0900

    fix: 결제 시간 랜덤값으로 변경
    테스트시 편리함을 위해 고정값을 사용하다 다시 변경

--- golang-server/internal/services/paymentService.go
+++ golang-server/internal/services/paymentService.go
@@ -2,9 +2,13 @@ package services
 
 import (
 	"golang-server/internal/dto"
+	"math/rand"
 	"time"
 )
 
 func Payment(paymentRequest dto.PaymentRequest) {
-	time.Sleep(3 * time.Second)
+	n := rand.Intn(3)
+	delay := time.Duration(n+1) * time.Second
+	time.Sleep(delay)
+	//time.Sleep(3 * time.Second)
 }
